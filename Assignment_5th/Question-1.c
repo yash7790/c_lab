3c9dbc950677bb2b33dc42cc8b5deecdaba44829
yash7790 <yashusharma1923@gmail.com>
2026-09-15 22:31:38 +0530

    Update Question-1.c

--- Assignment_5th/Question-1.c
+++ Assignment_5th/Question-1.c
@@ -36,42 +36,4 @@ int main() {
         printf("Invalid year");
 
     return 0;
-}#include <stdio.h>
-
-int main() {
-    int day, month, year;
-
-    printf("Enter day, month and year: ");
-    scanf("%d %d %d", &day, &month, &year);
-
-    if (year > 0) {
-        if (month >= 1 && month <= 12) {
-            if (month == 2) {
-                if (day >= 1 && day <= 28)
-                    printf("Valid date");
-                else if (day == 29 && (year % 400 == 0 || year % 4 == 0 && year % 100 != 0))
-                    printf("Valid date");
-                else
-                    printf("Invalid date");
-            }
-            else if (month == 4 || month == 6 || month == 9 || month == 11) {
-                if (day >= 1 && day <= 30)
-                    printf("Valid date");
-                else
-                    printf("Invalid date");
-            }
-            else {
-                if (day >= 1 && day <= 31)
-                    printf("Valid date");
-                else
-                    printf("Invalid date");
-            }
-        }
-        else
-            printf("Invalid month");
-    }
-    else
-        printf("Invalid year");
-
-    return 0;
 }
